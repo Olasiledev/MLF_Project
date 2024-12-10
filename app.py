919b0f8816ab9cb1043c45b93103e823cad8b725
Olasiledev <olasileoladipupo@gmail.com>
2024-12-09 19:42:51 -0500

    Add Procfile and start script for Render

--- app.py
+++ app.py
@@ -69,6 +69,6 @@ def predict_stock_price():
 
 
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 10000)) 
-#     app.run(debug=True, host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, host="0.0.0.0", port=port)
